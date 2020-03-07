4476c3c43a1e79e35c58be5eddd8281a9bd8c74f
Deyou <50372358+deyouzhang@users.noreply.github.com>
2020-03-07 19:17:49 +0800

    Update t219.py

--- t219.py
+++ t219.py
@@ -1,12 +1,16 @@
 import time
 print(time.time())
 
+print('hello deyou')
+
 import numpy as np
 print(np.__version__)
 
 with open('219.csv') as f:
     for i in f:
         print(i)
+
+
 
 
         
